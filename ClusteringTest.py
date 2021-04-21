7ad8f14a2c39960f0cb5f79cd74093720886e25f
Maximiliano Gonzalez Pazo <silver.maxi7@gmail.com>
2021-04-21 11:00:11 -0300

    Clustering and Degree Tests

--- ClusteringTest.py
+++ ClusteringTest.py
@@ -5,12 +5,19 @@ import pylab
   
 g = nx.Graph()
 
-g.add_edge(2, 1)
-g.add_edge(2, 2)
+g.add_edge(1, 2)
+g.add_edge(1, 3)
+g.add_edge(1, 4)
+g.add_edge(1, 5)
+
 g.add_edge(2, 3)
 g.add_edge(2, 4)
 g.add_edge(2, 5)
-g.add_edge(3, 6)
+
+g.add_edge(3, 4)
+g.add_edge(3, 5)
+
+g.add_edge(4, 5)
   
 nx.draw(g, with_labels = True)
 pylab.show()
