518dc36d326988373ace4d566f8af910223ebd0d
Maximiliano Gonzalez Pazo <silver.maxi7@gmail.com>
2021-04-24 17:47:32 -0300

    Mejora sobre el network Graph

--- NetworkGraph.py
+++ NetworkGraph.py
@@ -2,25 +2,53 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import pylab
-  
-g = nx.Graph()
 
-g.add_node(1)
-g.add_node(2)
-g.add_node(3)
-g.add_node(4)
-g.add_node(5)
-g.add_node(6)
+G = nx.MultiGraph()
+G.add_node(1)
+G.add_node(2)
+G.add_node(3)
+G.add_node(4)
+G.add_node(5)
+G.add_node(6)
+G.add_edge(1, 2)
+G.add_edge(1, 2)
+G.add_edge(1, 3)
+G.add_edge(1, 6)
+G.add_edge(2, 4)
+G.add_edge(4, 5)
+G.add_edge(4, 5)
+G.add_edge(6, 5)
 
-g.add_edge(1, 2)
-g.add_edge(1, 2)
-g.add_edge(1, 3)
-g.add_edge(1, 6)
-g.add_edge(2, 4)
-g.add_edge(4, 5)
-g.add_edge(4, 5)
-g.add_edge(6, 5)
-  
-nx.draw_circular(g, with_labels = True)
-nx.degree_centrality(g)
+pos = nx.random_layout(G)
+
+nx.draw_networkx_nodes(G, pos, node_color = 'r', node_size = 400, alpha = 1)
+ax = plt.gca()
+for e in G.edges:
+    ax.annotate(
+        "",
+        xy=pos[e[0]], 
+        xycoords='data',
+        xytext=pos[e[1]], 
+        textcoords='data',
+        arrowprops=dict
+            (
+                arrowstyle="-", 
+                color="0.5",
+                shrinkA=10, 
+                shrinkB=10,
+                patchA=None, 
+                patchB=None,
+                connectionstyle="arc3,rad=rrr".replace('rrr',str(0.3*e[2])),
+            ),
+        )
+
+for n in G.nodes:
+    ax.annotate(
+        n, 
+        xy=pos[n]-0.01, 
+        xycoords='data',
+        )
+
+plt.axis('off')
+plt.show()
 # %%
